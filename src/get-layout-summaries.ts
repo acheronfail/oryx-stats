fae15fbbc9572f8b931c3b213ba69979f0ca2134
acheronfail <acheronfail@gmail.com>
2021-09-02 17:44:15 +1000

    feat: do not redownload the layout-summaries every single time yarn start is run

--- src/get-layout-summaries.ts
+++ src/get-layout-summaries.ts
@@ -4,6 +4,12 @@ import { SearchLayoutsByTagsResponse, SearchLayoutsByTagsVariables } from './typ
 import { searchLatoutsByTagsVariables } from './options';
 
 (async function () {
+  if (await fs.promises.stat('layout-summaries.json').catch(() => false)) {
+    console.log('Looks like you have already downloaded the layout summaries, so skipping redownloading them');
+    console.log('If you want to redownload them again, just delete the layout-summaries.json file!');
+    return;
+  }
+
   const layoutSummaries = await request<SearchLayoutsByTagsResponse, SearchLayoutsByTagsVariables>(
     'https://oryx.zsa.io/graphql',
     gql`
